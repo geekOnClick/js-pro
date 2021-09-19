f4317f33ec92bd826156292fc6da7d38c6670715
Alexander Kozlov <kozloww1508@yandex.ru>
2021-09-19 19:34:16 +0300

    Выполнил 1 дз

--- scripts/cart.js
+++ scripts/cart.js
@@ -43,6 +43,7 @@ const goods = [
   const cartIcon = document.querySelector('.header__cart');
   let productsInCart = [];
   let totalPrice = 0; 
+  
 
   document.querySelectorAll('.productItem__addBtn').forEach((item) =>{
       item.addEventListener('click', (event) =>{
@@ -57,6 +58,7 @@ const goods = [
       })
   })
 
+    
 
 function changeNumberOfItems() {
     cartCount++;
@@ -93,6 +95,7 @@ function addItem(target){
         totalPrice += element.total;
     });
     document.getElementById('totalCost').textContent = totalPrice;
+
 }
 
 function getCurrentItem(target){
@@ -120,7 +123,7 @@ function addNewItem(item) {
     item.count = 1;
     item.total = item.count * item.price;
     productsInCart.push(item);
-    document.querySelector('.items').insertAdjacentHTML('afterbegin', `<div id = '${item.id}' class="item"> <p>${item.title}</p><p id='itemCount'>${item.count}</p><p>${item.price}.00$</p><p id='itemTotal'>${item.total}$</p><svg id='closeCart' width="10" height="10" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+    document.querySelector('.items').insertAdjacentHTML('afterbegin', `<div id = '${item.id}' class="item"> <p>${item.title}</p><p id='itemCount'>${item.count}</p><p>${item.price}.00$</p><p id='itemTotal'>${item.total}$</p><svg class='deleteItem' width="10" height="10" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path d="M11.2453 9L17.5302 2.71516C17.8285 2.41741 17.9962 2.01336 17.9966 1.59191C17.997 1.17045 17.8299 0.76611 17.5322 0.467833C17.2344 0.169555 16.8304 0.00177586 16.4089 0.00140366C15.9875 0.00103146 15.5831 0.168097 15.2848 0.465848L9 6.75069L2.71516 0.465848C2.41688 0.167571 2.01233 0 1.5905 0C1.16868 0 0.764125 0.167571 0.465848 0.465848C0.167571 0.764125 0 1.16868 0 1.5905C0 2.01233 0.167571 2.41688 0.465848 2.71516L6.75069 9L0.465848 15.2848C0.167571 15.5831 0 15.9877 0 16.4095C0 16.8313 0.167571 17.2359 0.465848 17.5342C0.764125 17.8324 1.16868 18 1.5905 18C2.01233 18 2.41688 17.8324 2.71516 17.5342L9 11.2493L15.2848 17.5342C15.5831 17.8324 15.9877 18 16.4095 18C16.8313 18 17.2359 17.8324 17.5342 17.5342C17.8324 17.2359 18 16.8313 18 16.4095C18 15.9877 17.8324 15.5831 17.5342 15.2848L11.2453 9Z" fill="#575757"/>
     </svg></div>`);
 }
@@ -131,5 +134,8 @@ function increaseItemValues(item){
     document.getElementById('itemCount').textContent = item.count;
     document.getElementById('itemTotal').textContent = `${item.total}$`;
 }
+
+
+
 
 
